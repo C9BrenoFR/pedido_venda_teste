2642461cce1ddc52345bcd7226013c79f883dff4
kidszoneWorld <ti.kz@kidszoneworld.com.br>
2024-12-09 17:00:54 -0300

    atualizando api

--- controllers/invoicesControllers.js
+++ controllers/invoicesControllers.js
@@ -4,26 +4,29 @@ const { getSpreadsheetData } = require('../utils/apiServiceLogistica');
 async function fetchLogisticsData(req, res) {
     try {
         // IDs do OneDrive para a planilha específica
-        const driveId = ''; // Substitua pelo ID correto do drive
-        const itemId = '';   // Substitua pelo ID correto do arquivo
+        const driveId = 'b!XH-eb-xz-E6X2UiiEteqUdmnlFRU0p5LmLmtxXKjZ0IQdp2f5MVEQqMjT7VUFk1Z'; // Substitua pelo ID correto do drive
+        const itemId = '01DKYK72E4OA25KFVSX5DIYRQ4A2BE57MQ';   // Substitua pelo ID correto do arquivo
         const sheetName = 'MASTER'; // Nome da aba na planilha
 
         const data = await getSpreadsheetData(driveId, itemId, sheetName);
 
         // Processar os dados para o formato necessário
         const formattedData = data.map(row => ({
-            dataEmissao: row[0],
-            numero: row[1],
+            NF: row[0],
+            EMISSÃO: row[1],
             codCliente: row[2],
-            cliente: row[3],
-            cnpj: row[4],
-            codRep: row[5],
-            representante: row[6],
-            transportadora: row[7],
-            saida: row[8],
-            previsaoEntrega: row[9],
-            agenda: row[10],
-            ocorrencia: row[11],
+            Rep: row[3],
+            NOME: row[4],
+            UF : row[5],
+            REGIÃO: row[6],
+            VOL: row[7],
+            CodTransporte: row[8],
+            TRANSPORTES: row[9],
+            SAÍDA: row[10],
+            PrevisaoEntrega: row[11],
+            ENTREGUE:row[12],
+            AGENDA  :row[13],
+            OCORRÊNCIA:row[14],
         }));
 
         res.json(formattedData);
